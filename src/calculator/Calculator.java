d84e6aceda7db521d3b1c7f12479d6feb8133573
Ghrafkly <69286273+Ghrafkly@users.noreply.github.com>
2022-07-29 18:46:05 +1000

    Commit

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -9,7 +9,7 @@ import java.util.Stack;
  * Program to calculate user expression
  *
  * @author Kyle
- * @version 1.0
+ * @version 1.1
  */
 public class Calculator implements ICalculator {
     /**
